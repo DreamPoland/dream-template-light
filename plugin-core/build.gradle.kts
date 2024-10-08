b7e4282d4df3d9f419773b5a47def154c2624c22
Ravis96 <ravis96@spoko.pl>
2024-10-08 18:32:44 +0200

    Bump dependencies

--- plugin-core/build.gradle.kts
+++ plugin-core/build.gradle.kts
@@ -9,12 +9,12 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
     // -- dream-platform --
-    implementation("cc.dreamcode.platform:core:1.12.8")
-    implementation("cc.dreamcode.platform:bukkit:1.12.8")
+    implementation("cc.dreamcode.platform:core:1.12.9")
+    implementation("cc.dreamcode.platform:bukkit:1.12.9")
 
     // -- dream-utilities --
-    implementation("cc.dreamcode:utilities:1.4.5")
-    implementation("cc.dreamcode:utilities-bukkit:1.4.5")
+    implementation("cc.dreamcode:utilities:1.5.1")
+    implementation("cc.dreamcode:utilities-bukkit:1.5.1")
 
     // -- injector --
     implementation("eu.okaeri:okaeri-injector:2.1.0")
